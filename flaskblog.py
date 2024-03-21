97aff36e50314bb45f2b267c73e1f5b1ff786d35
knnztrk <kenan.ozturk3@hotmail.com>
2024-03-21 23:29:51 +0300

    adding template block content, dummy data, template inheritance

--- flaskblog.py
+++ flaskblog.py
@@ -1,16 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
+
+# dummy data
+posts = [
+    {
+        'author': 'Tim Joe',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2024'
+    },
+    {
+        'author': 'Jhon Sim',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2024'
+    }
+]
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1>Hello, World! This is Home Page</h1>"
+    return render_template('index.html', posts=posts)
 
 @app.route('/about')
 def about():
-    return "<h1>About Page</h1>"
-
+    return render_template('about.html', title='About')
 
 if __name__ == '__main__':
     app.run(debug=True)
